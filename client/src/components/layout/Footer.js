16666a410a7fd2dc1dce5d20e3d2f539b0136618
archna-dev <63065633+archna-dev@users.noreply.github.com>
2020-07-25 21:28:59 -0700

    Landing page design change

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} Devgram
+    <footer className="bg-grey text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} InstaConnect
     </footer>
 
   )
